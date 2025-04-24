4a6ad4a8786f1946ad0423fd93d29e4dd79eeb68
Omar Guzman <Omar.Guzman-non-empl@moodys.com>
2025-04-24 13:17:57 -0600

    Fix path aliases for Heroku build

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,19 @@
-import type { NextConfig } from "next";
+// import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {
-  /* config options here */
+// const nextConfig: NextConfig = {
+//   /* config options here */
+// };
+
+// export default nextConfig;
+
+const path = require('path')
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  webpack: (config) => {
+    config.resolve.alias['@'] = path.resolve(__dirname);
+    return config;
+  },
 };
 
-export default nextConfig;
+module.exports = nextConfig;

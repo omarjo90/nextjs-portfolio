01b3b28485433e0479bc25475799a00044fecc37
Omar Guzman <Omar.Guzman-non-empl@moodys.com>
2025-04-24 13:23:34 -0600

    Fix webpack config alias typing for Heroku

--- next.config.ts
+++ next.config.ts
@@ -1,16 +1,8 @@
-// import type { NextConfig } from "next";
-
-// const nextConfig: NextConfig = {
-//   /* config options here */
-// };
-
-// export default nextConfig;
-
-const path = require('path')
+const path = require('path');
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  webpack: (config) => {
+  webpack: (/** @type {any} */ config) => {
     config.resolve.alias['@'] = path.resolve(__dirname);
     return config;
   },

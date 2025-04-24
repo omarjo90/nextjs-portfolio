5256ff7dacdd31a05ec5e5463ddd287f997b55fa
Omar Guzman <Omar.Guzman-non-empl@moodys.com>
2025-04-24 13:44:05 -0600

    Fix postcss plugin

--- postcss.config.js
+++ postcss.config.js
@@ -1,7 +1,8 @@
 // postcss.config.js
-import tailwindcss from "tailwindcss";
-import autoprefixer from "autoprefixer";
-
-export default {
-  plugins: [tailwindcss, autoprefixer],
-};
+module.exports = {
+    plugins: {
+      tailwindcss: {},
+      autoprefixer: {},
+    },
+  };
+  

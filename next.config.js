3ffccb2b087311557ad2942f4ffc4d53139ba96f
Omar Guzman <Omar.Guzman-non-empl@moodys.com>
2025-04-24 13:26:22 -0600

    Switch next.config to JS for Heroku compatibility

--- next.config.js
+++ next.config.js
@@ -2,7 +2,7 @@ const path = require('path');
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  webpack: (/** @type {any} */ config) => {
+  webpack: (config) => {
     config.resolve.alias['@'] = path.resolve(__dirname);
     return config;
   },
